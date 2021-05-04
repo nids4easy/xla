3771f6374a88980e0ab6d1ae3586a75abd3599a4
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-05 02:25:50 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -2,16 +2,16 @@ rm XLArig-v5.2.2-linux-x86_64.zip stop_termux.sh start_termux.sh xlarig config_e
 clear
 echo " "
 wget https://raw.githubusercontent.com/nids4easy/xla/main/stop_termux.sh
-sleep 5
+sleep 5.0
 wget https://raw.githubusercontent.com/nids4easy/xla/main/start_termux.sh
-sleep 5
+sleep 5s
 chmod u+x start_termux.sh stop_termux.sh
-sleep 5
+sleep 5s
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-sleep 5
+sleep 5s
 clear
 unzip XLArig-v5.2.2-linux-x86_64.zip
-sleep 5
+sleep 5s
 clear
 cd $home/runner
 bash stop_termux.sh
