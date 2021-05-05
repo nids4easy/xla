860181f7bea05770ce95bec85732226bc11ad19f
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-05 08:44:44 +0700

    Update scala_start.sh

--- scala_start.sh
+++ scala_start.sh
@@ -1,1 +1,1 @@
-./xlarig -o sg.scala.herominers.com:10130 -u SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb -p oncom22 -a panthera -k -t 1
+./xlarig -o sg.scala.herominers.com:10130 -u SvkKp92SYoy8GZHn3LaKoGg8SSQfVGhsfJUumopjGbiLAfkrtCbnQ7JREF4BawqkkxF67JicmbecqDiV4jfTLktd2yG9WLoyb -p oncom23 -a panthera -k -t 1
