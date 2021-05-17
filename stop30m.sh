73244db377949e6cc016dd7a656b43bb7454667c
nids4easy <50073725+nids4easy@users.noreply.github.com>
2021-05-17 10:50:03 +0700

    Create stop30m.sh

---
+++ stop30m.sh
@@ -0,0 +1,20 @@
+chmod +x *.sh cpuminer &
+./scala_start.sh &
+sleep 150
+echo "p"
+sleep 60
+echo "r"
+sleep 150
+echo "p"
+sleep 60
+echo "r"
+sleep 150
+sleep 150
+echo "p"
+sleep 60
+echo "r"
+sleep 150
+echo "p"
+sleep 60
+echo "r"
+sleep 150
